caf81d106c30f81535872e61c47fb250bb655f51
SooHee Choi <141746836+erika0915@users.noreply.github.com>
2024-10-25 21:22:52 +0900

    Title: 입력받아 계산, Time: 118ms, Memory: 10MB, Status: Passed - CodeTree

--- 241025/입력받아 계산/input-calculate.java
+++ 241025/입력받아 계산/input-calculate.java
@@ -4,8 +4,9 @@ public class Main {
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
-        int a = nextInt();
-        System.out.print( a+ 2);
+        int a = sc.nextInt();
+        int b  = a + 2;
+        System.out.print(b);
 
     }
 }
