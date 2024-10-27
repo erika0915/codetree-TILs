5f773f5be81a719aa6ae1cb41830d9f40e22a95b
SooHee Choi <141746836+erika0915@users.noreply.github.com>
2024-10-27 14:45:46 +0900

    Title: 장학금, Time: 123ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 241027/장학금/scholarship.java
+++ 241027/장학금/scholarship.java
@@ -5,12 +5,15 @@ public class Main {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
+        int b = sc.nextInt();
 
-        if (a>=95 && a<=100)
-            System.out.print("100000");
-        else if(a>=90)
+
+        if(a>=90 && b>=90)
             System.out.print("50000");
-        else
+        else if(a>=90 && b>=95)
+            System.out.print("100000");
+        else 
             System.out.print("0");
+        
     }
 }
