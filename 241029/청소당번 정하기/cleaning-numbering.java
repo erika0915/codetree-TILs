5d3add25e1232755d170eeccd966e0f7553526db
SooHee Choi <141746836+erika0915@users.noreply.github.com>
2024-10-29 00:07:16 +0900

    Title: 청소당번 정하기, Time: 125ms, Memory: 10MB, Status: Passed - CodeTree

--- 241029/청소당번 정하기/cleaning-numbering.java
+++ 241029/청소당번 정하기/cleaning-numbering.java
@@ -5,46 +5,16 @@ public class Main {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        int cnt2 = 0, cnt3 = 0, cnt12 = 0;
 
-        int cnt = 0;
-        int cnt1 = 0;
-        int cnt2 = 0;
-
-        for (int i = 1; i<=n; i++){
-            
-            if(i%2==0){
-                cnt++;
-            }
-            if(i%3==0){
-                cnt1++;
-            }
-            if(i%2==0 && i%3==0){
-                cnt--;
-                cnt1--;
-            }
-            if(i%6==0){
-                cnt1++;
-            }
-            if(i%2==0 && i%12==0){
-                cnt--;
-                cnt2--;
-            }
-            if(i%12==0){
+        for(int i = 1; i <= n; i++){
+            if(i%12==0)
+                cnt12++;
+            else if(i%3==0)
+                cnt3++;
+            else if(i%2==0)
                 cnt2++;
-            }
-            if(i%3==0 && i%12==0){
-                cnt1--;
-                cnt2--;
-            }
-            if(i%2==0 && i%3==0 && i%12==0){
-                cnt--;
-                cnt1--;
-                cnt2--;
-            }
-            if(i%24==0){
-                cnt2++;
-            } 
         }
-        System.out.print(cnt + " " + cnt1 + " " + cnt2);
+        System.out.println(cnt2 + " " + cnt3 + " " + cnt12);
     }
 }
