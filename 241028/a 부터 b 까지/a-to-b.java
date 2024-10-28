f7d20054b94731dfed96c5acd6a24f83355ec3e7
SooHee Choi <141746836+erika0915@users.noreply.github.com>
2024-10-28 16:14:59 +0900

    Title: a 부터 b 까지, Time: 127ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241028/a 부터 b 까지/a-to-b.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.print(a + " ");
+        for(int i =a; i<b;){
+            if(i%2==0){
+                i+=3;
+                System.out.print(i+" ");
+            }
+            else{
+                i*=2;
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
