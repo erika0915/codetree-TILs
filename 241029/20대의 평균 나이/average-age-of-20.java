ff6eba8d407f642b9bdf83f6d83786b875615eb0
SooHee Choi <141746836+erika0915@users.noreply.github.com>
2024-10-29 18:13:56 +0900

    Title: 20대의 평균 나이, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 241029/20대의 평균 나이/average-age-of-20.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int sum = 0;
+        double avg = 0;
+        int cnt = 0;
+
+        while(true){
+            int a = sc.nextInt();
+            sum+=a;
+            cnt++;
+
+            if(a>=30){
+                cnt--;
+                sum-=a;
+                break;
+            }
+        }
+        avg = (double)sum/cnt;
+        System.out.printf("%.2f", avg);
+    }
+}
