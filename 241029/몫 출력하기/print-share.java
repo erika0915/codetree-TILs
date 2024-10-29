7b458ca5ca76b1dbc25790ec772fc898a38c874e
SooHee Choi <141746836+erika0915@users.noreply.github.com>
2024-10-29 15:42:02 +0900

    Title: 몫 출력하기, Time: 137ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241029/몫 출력하기/print-share.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int cnt=0;
+
+        while(true){
+            int n = sc.nextInt();
+
+            if(n%2==0){
+                n/=2;
+                cnt++;
+                System.out.println(n);
+            }
+            if(cnt ==3){
+                break;
+            }
+        }
+    }
+}
