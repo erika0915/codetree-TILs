bca9d1482578870e94383d53781577568298aa66
SooHee Choi <141746836+erika0915@users.noreply.github.com>
2024-12-16 01:42:50 +0900

    Title: 특정 조건에 맞게 출력하기, Time: 124ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241216/특정 조건에 맞게 출력하기/print-in-specific-conditions.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int[] array = new int[20];
+        int index = 0;
+        
+        while(index < 20){
+            int input= sc.nextInt();
+            if(input==0){
+                break;
+            }
+
+            array[index]= input;
+
+            if(array[index]%2==0){
+                array[index] = array[index]/2;
+            }else{
+                array[index] = array[index]+3;
+            }
+
+            index++;
+        }
+        
+        for(int i= 0; i <index; i++){
+            System.out.print(array[i] + " ");
+        }
+    }
+}
