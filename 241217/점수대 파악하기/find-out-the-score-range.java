2afd4e398f562f6fe543a9650130cc3d31b599f2
SooHee Choi <141746836+erika0915@users.noreply.github.com>
2024-12-17 00:35:03 +0900

    Title: 점수대 파악하기, Time: 143ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241217/점수대 파악하기/find-out-the-score-range.java
@@ -0,0 +1,27 @@
+import java.util.Scanner; 
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int[] array = new int[100];
+        int[] countArray = new int[11];
+        int i= 0;
+
+        while(true){
+            array[i] = sc.nextInt();
+            if(array[i] == 0){
+                break;
+            }
+            i++;
+        }
+
+        for(int j =0; j <i; j++){
+            countArray[array[j]/10]++;
+        }
+
+        for(int k = 10; k >0; k--){
+            System.out.println(k*10 + " - " + countArray[k]);
+        }
+    }
+}
